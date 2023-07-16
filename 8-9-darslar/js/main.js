1764ce4185ea4cbdc7687e6598d81504cc5b329e
MuharramOzodboyeva <muharramozodboyeva598@gmail.com>
2023-07-16 11:15:40 +0500

    first commit

---
+++ 8-9-darslar/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+// alert("Hello javascript");
+// const placsCurrent = prompt("Where are you now");
+// console.log(placsCurrent);
+// const age = +prompt("How old are u?");
+// console.log(typeof age);
+// const favouriteColor = [];
+// favouriteColor[0] = prompt("whats your favourite color #1", "");
+// favouriteColor[1] = prompt("whats your favourite color #2", "");
+// favouriteColor[2] = prompt("whats your favourite color #3", "");
+// console.log(favouriteColor);
+// const now = confirm("Are u here", "");
+// console.log(now);
+// const favouritemovie = [];
+// favouritemovie[0] = prompt("What's your favourite movie #1", "");
+// favouritemovie[1] = prompt("What's your favourite movie #2", "");
+// favouritemovie[2] = prompt("What's your favourite movie #3", "");
+// console.log(favouritemovie);
+
+//interpolatsiya//c
+// const channel = prompt("What's your favourite bloger");
+// console.log(`https://youtube.com/${channel}`);
+// const user = "Muharram";
+// const job = "programmist";
+// console.log(`user name is ${user},she is ${job} `);
+// const user = "shahruzbek";
+// const job = "businesmen";
+// console.log(`user name is${user},he is a ${job}`);
---
+++ js/main.js
@@ -0,0 +1,40 @@
+"use strict";
+// let number = 10;
+// const borderColor = "muharram";
+// console.log(number);
+// console.log(borderColor);
+// const person = {
+//   name: "Muharram",
+// };
+// person.name = "yunus";
+// console.log(person);
+//biz o'zgaruvchilarni o'zgartira olamiz
+// const person = {
+//   name: "Apple",
+// };
+// person.name = "Banana";
+// console.log(person);
+// const mydream = {
+//   called: "Korea",
+// };
+// mydream.called = "London";
+// console.log(mydream);
+//7-dars
+// o'zgaruvchiilar
+// let number = 10;
+// console.log(number);
+// let text = "global";
+// console.log(text);
+// let isMerried = false;
+// let isprogrammaer = true;
+// console.log(isMerried);
+// console.log(isprogrammaer);
+// const greenBox
+// console.log(greenBox);
+// object
+const theif = {
+  age: 30,
+  jacket: "black",
+  isTall: false,
+};
+console.log(theif.jacket);
